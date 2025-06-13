95d12b47de54e3ac1cb262b50584e679de3d8943
salam <chaudharysm00@gmail.com>
2025-06-13 20:30:20 +0545

    upload middleware

---
+++ backend/src/middlewares/upload.js
@@ -0,0 +1,16 @@
+import multer from 'multer';
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
+import cloudinary from '../utils/cloudinary.js';
+
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: 'coffee-menu',
+    allowed_formats: ['jpg', 'png', 'jpeg', 'webp'],
+    transformation: [{ width: 500, height: 500, crop: 'limit' }],
+  },
+});
+
+const upload = multer({ storage });
+
+export default upload;
